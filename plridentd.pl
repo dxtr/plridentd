a7bcce6a5802982fd90ae761d13c4f4875c106b5
Kim Lidström <kim.lidas+github@gmail.com>
2012-03-16 16:39:46 +0100

    I think it's kind of finished now. At least ready for testing.

--- plridentd.pl
+++ plridentd.pl
@@ -5,10 +5,23 @@ package PlRIdentd;
 use strict;
 use warnings;
 use Net::Server::Fork;
+use Sys::Syslog;
 
 our @ISA = qw(Net::Server::Fork);
 
-__PACKAGE__->run();
+__PACKAGE__->run(pid_file => '/var/run/plridentd.pid',
+	log_file => 'Sys::Syslog',
+	syslog_logsoc => 'native',
+	syslog_ident => 'plridentd',
+	port => 113,
+	chroot => '/var/lib/plridentd/',
+	user => 'nobody',
+	group => 'nogroup',
+	background => 1,
+	setsid => 1,
+	max_servers => 10,
+	host => "0.0.0.0"
+);
 
 ### Override subs
 sub process_request
@@ -19,12 +32,13 @@ sub process_request
 		my $reply = '';
 		$reply .= ((0..9,'a'..'f')[rand(16)]) for 1..8;
 
-		my $timeout = 15;
+		my $timeout = 10;
 		my $previous_alarm = alarm($timeout);
 
 		while (<STDIN>) {
 			if ($_ =~ m/^\s*([\d]{1,5}, [\d]{1,5})\s*$/aa) {
 				print "$1 : USERID : UNIX : $reply\r\n";
+				last;
 			}
 		}
 		alarm($previous_alarm);
